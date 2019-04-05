8cd78cd68d7c99dc4dab6b6c815377c759b148d3
thorbaldin <49287452+thorbaldin@users.noreply.github.com>
2019-04-05 18:16:35 -0400

    Add files via upload

---
+++ traffic_counter_datalogger.ino
@@ -0,0 +1,212 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+// A simple data logger for the Arduino analog pins
+
+// how many milliseconds between grabbing data and logging it. 1000 ms is once a second
+#define LOG_INTERVAL  1000 // mills between entries (reduce to take more/faster data)
+
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+#define ECHO_TO_SERIAL   1 // echo data to serial port
+#define WAIT_TO_START    0 // Wait for serial input in setup()
+
+// the digital pins that connect to the LEDs
+#define redLEDpin 2
+#define greenLEDpin 3
+
+
+
+
+RTC_DS1307 RTC; // define the Real Time Clock object
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  
+  // red LED indicates error
+  digitalWrite(redLEDpin, HIGH);
+
+  while(1);
+}
+// from traffic counter sketch
+int sensorPin = A0;    
+int ledPin = 11;      
+int sensorValue = 0;  
+float averageValue = 0;
+int count=0;
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println();
+  
+  // use debugging LEDs
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+ 
+  pinMode(ledPin, OUTPUT);  
+  digitalWrite(sensorPin, HIGH);
+  
+#if WAIT_TO_START
+  Serial.println("Type any character to start");
+  while (!Serial.available());
+#endif //WAIT_TO_START
+
+  // initialize the SD card
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+  // connect to RTC
+  Wire.begin();  
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+  }
+  
+
+  logfile.println("datetime,count");    
+#if ECHO_TO_SERIAL
+  Serial.println("datetime,count");
+#endif //ECHO_TO_SERIAL
+ 
+  // If you want to set the aref to something other than 5v
+  //analogReference(EXTERNAL);
+}
+
+void loop(void)
+{
+  DateTime now;
+
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+  
+  digitalWrite(greenLEDpin, HIGH);
+  
+    
+
+
+  
+
+// read the value from the sensor:
+  sensorValue = analogRead(sensorPin);  
+  
+  if(millis()<2000)
+  {
+      averageValue +=((float)sensorValue-averageValue)/10.f; 
+      
+      digitalWrite(ledPin,HIGH);    
+  }else 
+  {
+    
+    if(sensorValue>averageValue +4){
+      count++;
+
+
+      // fetch the time
+  now = RTC.now();
+  // log time
+  
+  logfile.print('"');
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+  logfile.print('"');
+  logfile.print(", ");
+  logfile.print(count);
+  logfile.println(", ");
+  logfile.flush();
+#if ECHO_TO_SERIAL
+ 
+  Serial.print('"');
+  Serial.print(now.year(), DEC);
+  Serial.print("/");
+  Serial.print(now.month(), DEC);
+  Serial.print("/");
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+  Serial.print('"');
+  Serial.print(", ");
+  Serial.print(count);
+  Serial.print(", ");
+#endif //ECHO_TO_SERIAL
+
+
+      
+      averageValue =sensorValue+10;
+
+      digitalWrite(ledPin,HIGH);
+      delay(500);
+    }
+    else 
+    {
+    
+        digitalWrite(ledPin,LOW);
+        averageValue +=((float)sensorValue-averageValue)/100.f; 
+    }
+       
+  }
+  
+
+
+  digitalWrite(greenLEDpin, LOW);
+}  
+  
+
+
+  
